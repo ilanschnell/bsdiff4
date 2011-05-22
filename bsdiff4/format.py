0a36d0396b208e68b5f3e1d7bf38913a8770bb01
Ilan Schnell <ischnell@enthought.com>
2011-05-22 12:19:55 -0500

    docstings

--- bsdiff4/format.py
+++ bsdiff4/format.py
@@ -5,7 +5,7 @@ import core
 
 
 def diff(src, dst):
-    """Generate a BSDIFF4-format patch from 'src' to 'dst'.
+    """generate a BSDIFF4-format patch from 'src' to 'dst'
     """
     tcontrol, bdiff, bextra = core.diff(src, dst)
     # write control tuples as series of offts
@@ -25,17 +25,14 @@ def diff(src, dst):
 
 
 def patch(src, patch):
-    """Apply a BSDIFF4-format patch to the given string.
-
-    This function returns the result of applying the BSDIFF4-format patch
-    'patch' to the input string 'src',
+    """apply the BSDIFF4-format 'patch' to 'src'
     """
     magic = patch[:8]
     assert magic.startswith('BSDIFF4')
     # read the length headers
     l_bcontrol = core.decode_offt(patch[8:16])
     l_bdiff = core.decode_offt(patch[16:24])
-    l_target = core.decode_offt(patch[24:32])
+    l_dst = core.decode_offt(patch[24:32])
     # read the three data blocks
     bcontrol = bz2.decompress(patch[32:32 + l_bcontrol])
     bdiff = bz2.decompress(patch[32 + l_bcontrol:32 + l_bcontrol + l_bdiff])
@@ -46,4 +43,4 @@ def patch(src, patch):
                  core.decode_offt(bcontrol[i + 16:i + 24]))
                 for i in xrange(0, len(bcontrol), 24)]
     # actually do the patching
-    return core.patch(src, l_target, tcontrol, bdiff, bextra)
+    return core.patch(src, l_dst, tcontrol, bdiff, bextra)

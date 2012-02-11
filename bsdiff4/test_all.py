75827727808889e0ee53893e697882b0c03a02ac
Ilan Schnell <ischnell@enthought.com>
2012-02-10 21:17:14 -0600

    support Python 2.5 again

--- bsdiff4/test_all.py
+++ bsdiff4/test_all.py
@@ -1,10 +1,19 @@
 import os
+import sys
 import random
 import unittest
 
 import bsdiff4.core as core
 import bsdiff4.format as format
 
+
+def to_bytes(s):
+    if sys.version_info[0] >= 3:
+        return bytes(s.encode('latin1'))
+    elif sys.version_info[:2] >= (2, 6):
+        return bytes(s)
+    else:
+        return s
 
 N = 2 ** 63 - 1
 
@@ -17,28 +26,28 @@ class TestEncode(unittest.TestCase):
 
     def test_special_values(self):
         for n, s in [
-            (-N, b'\xff\xff\xff\xff\xff\xff\xff\xff'),
-            (-256, b'\x00\x01\x00\x00\x00\x00\x00\x80'),
-            (-128, b'\x80\x00\x00\x00\x00\x00\x00\x80'),
-            (-1, b'\x01\x00\x00\x00\x00\x00\x00\x80'),
-            (0, b'\x00\x00\x00\x00\x00\x00\x00\x00'),
-            (1, b'\x01\x00\x00\x00\x00\x00\x00\x00'),
-            (127, b'\x7f\x00\x00\x00\x00\x00\x00\x00'),
-            (128, b'\x80\x00\x00\x00\x00\x00\x00\x00'),
-            (129, b'\x81\x00\x00\x00\x00\x00\x00\x00'),
-            (255, b'\xff\x00\x00\x00\x00\x00\x00\x00'),
-            (256, b'\x00\x01\x00\x00\x00\x00\x00\x00'),
-            (257, b'\x01\x01\x00\x00\x00\x00\x00\x00'),
-            (N, b'\xff\xff\xff\xff\xff\xff\xff\x7f'),
+            (-N, '\xff\xff\xff\xff\xff\xff\xff\xff'),
+            (-256, '\x00\x01\x00\x00\x00\x00\x00\x80'),
+            (-128, '\x80\x00\x00\x00\x00\x00\x00\x80'),
+            (-1, '\x01\x00\x00\x00\x00\x00\x00\x80'),
+            (0, '\x00\x00\x00\x00\x00\x00\x00\x00'),
+            (1, '\x01\x00\x00\x00\x00\x00\x00\x00'),
+            (127, '\x7f\x00\x00\x00\x00\x00\x00\x00'),
+            (128, '\x80\x00\x00\x00\x00\x00\x00\x00'),
+            (129, '\x81\x00\x00\x00\x00\x00\x00\x00'),
+            (255, '\xff\x00\x00\x00\x00\x00\x00\x00'),
+            (256, '\x00\x01\x00\x00\x00\x00\x00\x00'),
+            (257, '\x01\x01\x00\x00\x00\x00\x00\x00'),
+            (N, '\xff\xff\xff\xff\xff\xff\xff\x7f'),
             ]:
-            self.assertEqual(core.encode_int64(n), s)
-            self.assertEqual(core.decode_int64(s), n)
+            b = to_bytes(s)
+            self.assertEqual(core.encode_int64(n), b)
+            self.assertEqual(core.decode_int64(b), n)
 
     def test_errors(self):
         self.assertRaises(TypeError, core.encode_int64, 'x')
         self.assertRaises(TypeError, core.decode_int64, 12345)
-        self.assertRaises(ValueError, core.decode_int64, b'1234567')
-        self.assertRaises(ValueError, core.decode_int64, b'123456789')
+        self.assertRaises(ValueError, core.decode_int64, to_bytes('1234567'))
 
     def test_random(self):
         for dum in range(1000):
@@ -57,7 +66,7 @@ class TestFormat(unittest.TestCase):
         self.assertEqual(dst, dst2)
 
     def test_zero(self):
-        self.round_trip(b'', b'')
+        self.round_trip(to_bytes(''), to_bytes(''))
 
     def test_extra(self):
         src = gen_random_bytes(1000)

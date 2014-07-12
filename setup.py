4f8f3542a77b0f4773de1bc4265817b5d658e786
Ilan Schnell <ilanschnell@gmail.com>
2014-07-12 13:49:30 -0500

    add Python 3.4 as officially supported

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Utilities",
     ],
     description = "binary diff and patch using the BSDIFF4-format",

089904dff5355808b5c64dabe97c7a63da4aafb8
Ilan Schnell <ilanschnell@gmail.com>
2020-07-18 21:04:48 -0500

    typo

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Utilities",
     ],
     description = "binary diff and patch using the BSDIFF4-format",

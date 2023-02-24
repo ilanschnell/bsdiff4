e05c387c856f66c73cebebe45d08030a0ca78ae1
Ilan Schnell <ilanschnell@gmail.com>
2023-02-24 07:46:47 -0600

    next release 1.2.4

--- bsdiff4/__init__.py
+++ bsdiff4/__init__.py
@@ -4,7 +4,7 @@ from bsdiff4.format import (diff, patch, file_diff, file_patch,
                             file_patch_inplace)
 
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 
 def test(verbosity=1):
